4acd61f47502419540e575314a9262f38b8203be
Yurii Holiuk <holiuk.yurii@pdffiller.team>
2019-06-14 21:41:29 +0300

    add datepicker

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Api } from './api';
+import { createDateString, createFlightMapper } from './utils';
+import { BASE_URL, COLUMNS_BY_TYPE, FLIGHT_TYPES } from './constants';
+import { Button } from './components/Button';
+import { FlightRow } from './components/FlightRow';
 
-const BASE_URL = 'https://api.iev.aero/api/flights';
-
-
-/*
-* Terminal - term
-* Gate - gateNo
-* Time - timeDepSchedule
-* Destination - airportToID.name_en
-* Airline  - airline.en.name
-* Flight # - codeShareData.codeShare
-* Status - status
-*
-* timeArrSchedule — для arrivals.
-* arrivals Destination - airportFromID.name_en
-* id - id
-* */
+const { ARRIVAL, DEPARTURE } = FLIGHT_TYPES;
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.api = new Api(BASE_URL);
 
-    const today = new Date();
-    const monthDay = today.getDate().toString().padStart(2, '0');
-    const month = (today.getMonth() + 1).toString().padStart(2, '0');
-    const year = today.getFullYear();
-
     this.state = {
-      selectedDate: `${monthDay}-${month}-${year}`,
-      departures: [],
-      arrivals: [],
+      selectedDate: createDateString(new Date()),
+      [DEPARTURE]: [],
+      [ARRIVAL]: [],
+      activeFlightType: DEPARTURE,
     };
   }
 
@@ -42,49 +27,84 @@ export class App extends Component {
     this.loadFlights(selectedDate);
   }
 
+  changeActiveFlight = type => (event) => {
+    event.preventDefault();
+
+    this.setState({ activeFlightType: type });
+  };
+
+  selectDate = ({ target: { value } }) => {
+    const [year, month, day] = value.split('-');
+    const selectedDate = [day, month, year].join('-');
+
+    this.loadFlights(selectedDate);
+
+    this.setState({
+      selectedDate,
+      [DEPARTURE]: [],
+      [ARRIVAL]: [],
+    });
+  };
+
   async loadFlights(date) {
     const data = await this.api.get(date);
     const { departure, arrival } = data.body;
-    const departures = departure.map((departureItem) => {
-      return {
-        terminal: departureItem.term,
-        gate: departureItem.gateNo,
-        destination: departureItem['airportToID.name_en'],
-        airline: departureItem.airline
-          && departureItem.airline.en
-          && departureItem.airline.en.name,
-        flightNumber: departureItem.codeShareData
-          && departureItem.codeShareData[0]
-          && departureItem.codeShareData[0].codeShare,
-        status: departureItem.status,
-        time: departureItem.timeDepShedule,
-      };
-    });
-    const arrivals = arrival.map((arrivalItem) => {
-      return {
-        terminal: arrivalItem.term,
-        gate: arrivalItem.gateNo,
-        destination: arrivalItem['airportToID.name_en'],
-        airline: arrivalItem.airline
-          && arrivalItem.airline.en
-          && arrivalItem.airline.en.name,
-        flightNumber: arrivalItem.codeShareData
-          && arrivalItem.codeShareData[0]
-          && arrivalItem.codeShareData[0].codeShare,
-        status: arrivalItem.status,
-        time: arrivalItem.timeArrSchedule,
-      };
-    });
 
-    this.setState({ departures, arrivals });
+    const departures = departure.map(createFlightMapper(DEPARTURE));
+    const arrivals = arrival.map(createFlightMapper(ARRIVAL));
+
+    this.setState({ [DEPARTURE]: departures, [ARRIVAL]: arrivals });
   }
 
   render() {
-    const { departures, arrivals } = this.state;
+    const { activeFlightType, selectedDate } = this.state;
+
+    const [day, month, year] = selectedDate.split('-');
+    const pickerValue = [year, month, day].join('-');
 
     return (
-      <div>
-        <pre>{JSON.stringify(arrivals, null, 4)}</pre>
+      <div className="container">
+        <div className="controls">
+          <Button
+            isActive={activeFlightType === DEPARTURE}
+            onClick={this.changeActiveFlight(DEPARTURE)}
+          >
+            Departures
+          </Button>
+
+          <Button
+            isActive={activeFlightType === ARRIVAL}
+            onClick={this.changeActiveFlight(ARRIVAL)}
+          >
+            Arrivals
+          </Button>
+        </div>
+
+        <div className="calendar">
+          <input
+            type="date"
+            value={pickerValue}
+            onChange={this.selectDate}
+          />
+        </div>
+
+        <table className="flights">
+          <thead>
+            <tr>
+              {COLUMNS_BY_TYPE[activeFlightType].map(columnName => (
+                <th key={columnName}>
+                  <span>{columnName}</span>
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state[activeFlightType].map(flight => (
+              <FlightRow key={flight.id} type={activeFlightType} {...flight} />
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
